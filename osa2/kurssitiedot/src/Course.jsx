798fb90c41a5e616eb29dec411f56e897c3146ec
Niko Heikkila <nikoheikkilat@gmail.com>
2025-05-05 18:37:01 +0300

    Tehtävät 2.1-2.5

---
+++ osa2/kurssitiedot/src/Course.jsx
@@ -0,0 +1,45 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => {
+    return sum + part.exercises;
+  }, 0);
+
+  return (
+    <p>
+      <strong>Total of {totalExercises} exercises</strong>
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
